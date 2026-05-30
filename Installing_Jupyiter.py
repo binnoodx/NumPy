13bdbaa8250b7dfadcb589a4951192606cf0a01d
Binod Sharma <thebinod404@gmail.com>
2026-05-30 13:10:24 +0545

    Reinitialize the github repo

---
+++ Installing_Jupyiter.py
@@ -0,0 +1,6 @@
+#pip install numpy notebook
+
+#type jupyter notebook in terminal to open the jupyter notebook in browser
+
+#to create a new notebook click on new and select python 3
+
---
+++ Introduction.py
@@ -0,0 +1,28 @@
+# Introduction to Numpy
+
+#Numpy is low level library written in C and Fortran. It is used for scientific computing and data analysis. It provides a powerful N-dimensional array object, and tools for working with these arrays.
+
+#Numpy eliminates the slower algorithms of python
+
+# At Past time , Python was not meant to be used in machine learning and data science because of its slow speed but Numpy was introduced with speed and performance of C and Comfort of Python. It is used for linear algebra, Fourier transform, and random number generation.
+
+#-------------------------------------------
+
+#Features of Numpy:
+#1. N-dimensional array object (ndarray)
+#2. Broadcasting functions
+#3. Tools for integrating C/C++ and Fortran code
+#4. Useful linear algebra, Fourier transform, and random number capabilities
+
+#--------------------------------------------
+
+#Normal Arrays
+# -- Heterogeneous data types
+# -- Variable size
+# -- No built-in mathematical functions
+
+
+#N-d Arrays
+# -- Homogeneous data types
+# -- Fixed size
+# -- Built-in mathematical functions
